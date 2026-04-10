30709ed485e7234eecb176af7fa1c8e41014e739
Diego Alejandro Mallama Yandun <diego.mallamay@campusucc.edu.co>
2026-04-09 23:22:11 -0500

    add interfaces

---
+++ src/main/java/com/universidad/universidad/interfaces/Autenticable.java
@@ -0,0 +1,6 @@
+package com.universidad.universidad.interfaces;
+
+public interface Autenticable {
+    boolean login(String usuario,String password);
+    
+}
---
+++ src/main/java/com/universidad/universidad/interfaces/Notificable.java
@@ -0,0 +1,5 @@
+package com.universidad.universidad.interfaces;
+
+public interface Notificable {
+    void enviarNotificacion(String mensaje);
+}
